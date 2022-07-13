d2556e915927ee4469143d6e5274d9c05da7336a
alissonkhr <alissonricardo68@gmail.com>
2022-07-13 16:52:00 -0700

    created class for player to be drawn onto canvas

--- js/app.js
+++ js/app.js
@@ -1,8 +1,31 @@
-const canvas = document.querySelector('canvas'); // grabs canvas element
-// console.log(canvas);
+const canvas = document.querySelector('canvas') // grabs canvas element
+// console.log(canvas)
 
 const c = canvas.getContext('2d') // this function is used to access the canvas tags 2D drawing functions that will be implemented later for the game
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight; // takes up width and height of window for now, may change later
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight // takes up width and height of window
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 200,
+            y: 200
+        } // where the player will be positioned on the canvas
+        
+        this.velocity = {
+            x: 0,
+            y: 0
+        } // how fast the player will move along the canvas axes
+
+        // this.image = none yet
+        this.width = 100
+        this.height = 100 // player will have box collision detection
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
 
