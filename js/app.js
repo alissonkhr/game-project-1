4105195f0b1b8eba64c884cd1e5104cfa767a9b5
alissonkhr <alissonricardo68@gmail.com>
2022-07-13 16:36:53 -0700

    canvas selector, height and width created

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
-console.log('JS connected');
+const canvas = document.querySelector('canvas'); // grabs canvas element
+// console.log(canvas);
+
+const c = canvas.getContext('2d') // this function is used to access the canvas tags 2D drawing functions that will be implemented later for the game
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight; // takes up width and height of window for now, may change later
 
