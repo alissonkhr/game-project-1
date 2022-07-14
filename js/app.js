6162082f24ce3526cd5cb4f4d74bfe49bcb49174
alissonkhr <alissonricardo68@gmail.com>
2022-07-13 17:59:28 -0700

    player image centered to bottom of screen

--- js/app.js
+++ js/app.js
@@ -1,50 +1,55 @@
-const canvas = document.querySelector('canvas') // grabs canvas element
+const canvas = document.querySelector("canvas"); // grabs canvas element
 // console.log(canvas)
 
-const c = canvas.getContext('2d') // this function is used to access the canvas tags 2D drawing functions that will be implemented later for the game
+const c = canvas.getContext("2d"); // this function is used to access the canvas tags 2D drawing functions that will be implemented later for the game
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight // takes up width and height of window
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight; // takes up width and height of window
 
 class Player {
-    constructor() {
-        this.position = {
-            x: 200,
-            y: 200
-        } // where the player will be positioned on the canvas
-        
-        this.velocity = {
-            x: 0,
-            y: 0
-        } // how fast the player will move along the canvas axes
+  constructor() {
+    this.velocity = {
+      x: 0,
+      y: 0,
+    }; // how fast the player will move along the canvas axes
 
-        const playerImage = new Image()
-        playerImage.src = './images/link.png'
-        playerImage.onload = () => {
-            const scale = 0.15
-            this.image = playerImage
-            this.width = playerImage.width * scale
-            this.height = playerImage.height * scale // player will have box collision detection
-        }
-    }
+    const playerImage = new Image();
+    playerImage.src = "./images/link.png";
+    playerImage.onload = () => {
+      const scale = 0.1;
+      this.image = playerImage;
+      this.width = playerImage.width * scale;
+      this.height = playerImage.height * scale;
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height - this.height,
+      }; // where the player will be positioned on the canvas
+    };
+  }
 
-    draw() {
-        // c.fillStyle = 'red'
-        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
-        if (this.image)
-        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
-    }
+  draw() {
+    // c.fillStyle = 'red'
+    // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    if (this.image)
+      c.drawImage(
+        this.image,
+        this.position.x,
+        this.position.y,
+        this.width,
+        this.height
+      );
+  }
 }
 
-const player = new Player() // instantiate player from Player class
+const player = new Player(); // instantiate player from Player class
 
-player.draw() // player is now on canvas
+player.draw(); // player is now on canvas
 
 function animatePlayer() {
-    requestAnimationFrame(animatePlayer)
-    c.fillStyle = 'tan'
-    c.fillRect(0, 0, canvas.width, canvas.height)
-    player.draw()
+  requestAnimationFrame(animatePlayer);
+  c.fillStyle = "tan";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.draw();
 }
 
-animatePlayer()
+animatePlayer();
